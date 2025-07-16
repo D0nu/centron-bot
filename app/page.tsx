4a3647fa416038db10a3ec1dd9a1d9aedb61d551
D0nu <ikechukwujoseph146@gmail.com>
2025-07-16 12:58:44 +0200

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Social from "@/components/social";
 
 export default function Home() {
   return (
-    <div className="relative overflow-hidden lg:-mt-48 ">
+    <div className="relative overflow-hidden lg:-mt-48">
       {/* Wrap content sections in a landscape-responsive container */}
       <div className="flex flex-col landscape-md:flex-col-reverse">
         {/* Text & Buttons Section - Should appear SECOND in landscape */}
@@ -23,24 +23,23 @@ export default function Home() {
                 speeds.
               </h2>
 
-              {/* H3 shows at 940px and above */}
-<h3 className="hidden desktop-940:block text-white text-[24.99px] leading-snug font-medium mt-4">
-  Unlock Features Like DCA, Multi-Wallet <br />
-  Support & Live Position Tracking
-</h3>
+              <h3 className="hidden desktop-940:block text-white text-[24.99px] leading-snug font-medium mt-4">
+                Unlock Features Like DCA, Multi-Wallet <br />
+                Support & Live Position Tracking
+              </h3>
             </div>
 
             <div className="flex flex-col lg:flex-row mt-14 gap-7 justify-center items-center max-w-6xl mx-auto r1024x600:mb-[280px]">
+              {/* Launch Bot Button */}
               <button className="w-[210px] lg:w-[250px] bg-[#3FFF90] text-black font-medium py-3 px-6 rounded-xl text-lg hover:bg-green-400 transition-colors duration-200 flex items-center justify-center gap-2">
-                {/* Launch Bot Icon */}
                 <svg width="20" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" clipRule="evenodd" d="M20.1908 0.660481C21.4064 0.148648 22.7096 1.17655 22.4949 2.47794L19.9597 17.8493C19.7153 19.3313 18.0875 20.1818 16.7278 19.4432C15.5899 18.8252 13.9018 17.8739 12.3804 16.8798C11.6207 16.3834 9.29446 14.7916 9.5804 13.6584C9.82488 12.6896 13.7356 9.0493 15.9703 6.8844C16.8481 6.03401 16.4483 5.54273 15.4116 6.32572C12.8399 8.26804 8.71099 11.2211 7.34561 12.0522C6.14094 12.7854 5.51196 12.9106 4.7617 12.7854C3.39166 12.5572 2.12147 12.2036 1.08446 11.7738C-0.317132 11.1928 -0.248861 9.26663 1.08351 8.70571L20.1908 0.660481Z" fill="black" />
                 </svg>
                 Launch Bot
               </button>
 
+              {/* Leaderboard Button */}
               <button className="w-[210px] lg:w-[250px] bg-[#3FFF90] text-black font-medium py-3 px-6 rounded-xl text-lg hover:bg-green-400 transition-colors duration-200 flex items-center justify-center gap-2">
-                {/* Leaderboard Icon */}
                 <svg width="26" height="21" viewBox="0 0 26 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g clipPath="url(#clip0_152_1017)">
                     <path d="M10.3952 0.11377H15.0775C15.5244 0.11377 15.8874 0.476684 15.8874 0.923618V20.1266H9.58594V0.923618C9.58594 0.476684 9.94885 0.11377 10.3958 0.11377H10.3952Z" fill="black" />
@@ -56,8 +55,13 @@ export default function Home() {
                 Leaderboard
               </button>
 
-              <button className="w-[210px] lg:w-[250px] bg-[#3FFF90] text-black font-medium py-3 px-6 rounded-xl text-lg hover:bg-green-400 transition-colors duration-200 flex items-center justify-center gap-2">
-                {/* Litepaper Icon */}
+              {/* Litepaper Button as Link */}
+              <a
+                href="https://pine-book-6a9.notion.site/Quick-Start-230e92ca1b6380c59749f9d18d11bbfb"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-[210px] lg:w-[250px] bg-[#3FFF90] text-black font-medium py-3 px-6 rounded-xl text-lg hover:bg-green-400 transition-colors duration-200 flex items-center justify-center gap-2 z-50"
+              >
                 <svg width="33" height="30" viewBox="0 0 33 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g clipPath="url(#clip0_152_1023)">
                     <path d="M8.02925 6.23145C8.90512 6.943 9.23369 6.8887 10.8784 6.77898L26.3838 5.84796C26.7125 5.84796 26.4391 5.51989 26.3295 5.46536L23.7543 3.60377C23.261 3.22071 22.6035 2.782 21.3437 2.89174L6.32979 3.9868C5.78226 4.0411 5.67288 4.31485 5.89096 4.53431L8.02925 6.23145ZM8.96017 9.84494V26.1593C8.96017 27.036 9.39831 27.3642 10.3845 27.3099L27.4249 26.3239C28.4115 26.2697 28.5214 25.6664 28.5214 24.9543V8.74942C28.5214 8.03831 28.248 7.65482 27.6439 7.70955L9.8365 8.74942C9.17935 8.80463 8.96017 9.13335 8.96017 9.84494ZM25.7824 10.7201C25.8917 11.2133 25.7824 11.7061 25.2883 11.7615L24.4672 11.9251V23.9693C23.7543 24.3525 23.097 24.5715 22.5493 24.5715C21.6722 24.5715 21.4526 24.2977 20.7957 23.4768L15.4251 15.0458V23.2032L17.1245 23.5866C17.1245 23.5866 17.1245 24.5715 15.7535 24.5715L11.9737 24.7908C11.8639 24.5715 11.9737 24.0246 12.3571 23.9149L13.3434 23.6416V12.8561L11.9739 12.7464C11.8641 12.2532 12.1376 11.5421 12.9053 11.4868L16.9602 11.2135L22.5493 19.7543V12.1988L21.1242 12.0353C21.0149 11.4323 21.4526 10.9945 22.0006 10.9402L25.7824 10.7201ZM5.06919 2.50868L20.6861 1.35865C22.6039 1.19417 23.0972 1.30435 24.3027 2.17994L29.2876 5.68369C30.1103 6.28621 30.3844 6.45024 30.3844 7.10706V26.3239C30.3844 27.5281 29.9457 28.2404 28.4117 28.3493L10.276 29.4446C9.12456 29.4995 8.57655 29.3353 7.97355 28.5683L4.30245 23.8054C3.64464 22.9287 3.37109 22.2727 3.37109 21.5052V4.42416C3.37109 3.43927 3.80993 2.61776 5.06919 2.50868Z" fill="black" />
@@ -69,35 +73,33 @@ export default function Home() {
                   </defs>
                 </svg>
                 Litepaper
-              </button>
+              </a>
             </div>
           </div>
         </div>
 
-        {/* Design/Social Section - Positioned higher at 867-1023px */}
-<div
-  className="
-    -mt-[520px] 
-    -mb-44
-
-    sm:-mt-[570px] 
-    md:-mt-[600px] 
-    lg:-mt-[550px] 
-
-    landscape:-mt-[180px]
-    landscape:sm:-mt-[600px]
-    landscape:md:-mt-[500px]
-    landscape:lg:-mt-[600px]
-  "
->
-  <Design />
-  <Social />
-</div>
-
+        {/* Design and Social Section */}
+        <div
+          className="
+            -mt-[520px] 
+            -mb-44
+            sm:-mt-[570px] 
+            md:-mt-[600px] 
+            r1024x1366:-mt-[800px]
+            lg:-mt-[550px] 
+            landscape:-mt-[180px]
+            landscape:sm:-mt-[600px]
+            landscape:md:-mt-[500px]
+            landscape:lg:-mt-[600px]
+          "
+        >
+          <Design />
+          <Social />
+        </div>
       </div>
 
-      {/* Fixed Footer at bottom */}
-      <div className="fixed bottom-0 left-0 w-full z-30">
+      {/* Fixed Footer */}
+      <div className="fixed bottom-0 left-0 w-full">
         <Footer />
       </div>
     </div>
--- components/social.tsx
+++ components/social.tsx
@@ -3,14 +3,19 @@
 
 export default function Social() {
   return (
-    <div className="relative w-full h-[400px]"> {/* Adjust height to fit your layout */}
-      {/* Social Bar */}
+    <div className="relative w-full h-[400px]">
       <div className="absolute top-[-100px] left-1/2 transform -translate-x-1/2 bg-[#161616] py-5 rounded-full 
       flex flex-wrap md:flex-nowrap justify-center lg:justify-between items-center 
       w-full max-w-[95%] sm:max-w-[90%] md:max-w-[70%] lg:max-w-[70%] gap-10 sm:gap-12 md:gap-14 
-      px-4 sm:px-6 md:px-12 text-white">
+      px-4 sm:px-6 md:px-12 text-white z-20">
 
         {/* Bot */}
+        <a
+          href="https://t.me/CentronBotCommunity"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary  font-medium"
+        >
         <div className="flex items-center gap-2 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
           <svg
             className="w-6 h-6 lg:w-6 lg:h-6"
@@ -27,64 +32,77 @@ export default function Social() {
           </svg>
           <h1 className="hidden desktop-940:block lg:block">Bot</h1>
         </div>
+        </a>
 
         {/* Notion */}
-        <div className="flex items-center gap-1 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
-          <svg
-            className="w-6 h-6 lg:w-6 lg:h-6"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <title>Notion</title>
-            <path
-              fill="#ffffff"
-              d="M4.459 4.208c.746.606 1.026.56 2.428.466l13.215-.793c.28 0 .047-.28-.046-.326L17.86 1.968c-.42-.326-.981-.7-2.055-.607L3.01 2.295c-.466.046-.56.28-.374.466zm.793 3.08v13.904c0 .747.373 1.027 1.214.98l14.523-.84c.841-.046.935-.56.935-1.167V6.354c0-.606-.233-.933-.748-.887l-15.177.887c-.56.047-.747.327-.747.933zm14.337.745c.093.42 0 .84-.42.888l-.7.14v10.264c-.608.327-1.168.514-1.635.514-.748 0-.935-.234-1.495-.933l-4.577-7.186v6.952L12.21 19s0 .84-1.168.84l-3.222.186c-.093-.186 0-.653.327-.746l.84-.233V9.854L7.822 9.76c-.094-.42.14-1.026.793-1.073l3.456-.233 4.764 7.279v-6.44l-1.215-.139c-.093-.514.28-.887.747-.933zM1.936 1.035l13.31-.98c1.634-.14 2.055-.047 3.082.7l4.249 2.986c.7.513.934.653.934 1.213v16.378c0 1.026-.373 1.634-1.68 1.726l-15.458.934c-.98.047-1.448-.093-1.962-.747l-3.129-4.06c-.56-.747-.793-1.306-.793-1.96V2.667c0-.839.374-1.54 1.447-1.632z"
-            />
-          </svg>
-          <h1 className="hidden desktop-940:block lg:block">Notion</h1>
-        </div>
+        <a
+          href="https://pine-book-6a9.notion.site/Quick-Start-230e92ca1b6380c59749f9d18d11bbfb"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary  font-medium"
+        >
+          <div className="flex items-center gap-1 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
+            <svg
+              className="w-6 h-6 lg:w-6 lg:h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <title>Notion</title>
+              <path
+                fill="#ffffff"
+                d="M4.459 4.208c.746.606 1.026.56 2.428.466l13.215-.793c.28 0 .047-.28-.046-.326L17.86 1.968c-.42-.326-.981-.7-2.055-.607L3.01 2.295c-.466.046-.56.28-.374.466zm.793 3.08v13.904c0 .747.373 1.027 1.214.98l14.523-.84c.841-.046.935-.56.935-1.167V6.354c0-.606-.233-.933-.748-.887l-15.177.887c-.56.047-.747.327-.747.933zm14.337.745c.093.42 0 .84-.42.888l-.7.14v10.264c-.608.327-1.168.514-1.635.514-.748 0-.935-.234-1.495-.933l-4.577-7.186v6.952L12.21 19s0 .84-1.168.84l-3.222.186c-.093-.186 0-.653.327-.746l.84-.233V9.854L7.822 9.76c-.094-.42.14-1.026.793-1.073l3.456-.233 4.764 7.279v-6.44l-1.215-.139c-.093-.514.28-.887.747-.933zM1.936 1.035l13.31-.98c1.634-.14 2.055-.047 3.082.7l4.249 2.986c.7.513.934.653.934 1.213v16.378c0 1.026-.373 1.634-1.68 1.726l-15.458.934c-.98.047-1.448-.093-1.962-.747l-3.129-4.06c-.56-.747-.793-1.306-.793-1.96V2.667c0-.839.374-1.54 1.447-1.632z"
+              />
+            </svg>
+            <h1 className="hidden desktop-940:block lg:block">Notion</h1>
+          </div>
+        </a>
 
         {/* Twitter/X */}
-<div className="relative z-10"> {/* Add z-index container */}
-  <a
-    href="https://x.com/CentronBot"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="text-primary hover:underline font-medium"
-    onClick={(e) => {
-      e.preventDefault();
-      window.open('https://x.com/CentronBot', '_blank');
-    }}
-  >
-    <div className="flex items-center gap-1 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
-      {/* Updated SVG */}
-      <svg 
-        className="w-6 h-6 lg:w-6 lg:h-6 text-white"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-      >
-        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-      </svg>
-      <h1 className="hidden desktop-940:block lg:block">Twitter(X)</h1>
-    </div>
-  </a>
-</div>
+        <div className="relative z-10">
+          <a
+            href="https://x.com/CentronBot"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary  font-medium"
+            
+          >
+            <div className="flex items-center gap-1 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
+              <svg 
+                className="w-6 h-6 lg:w-6 lg:h-6 text-white"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+              </svg>
+              <h1 className="hidden desktop-940:block lg:block">Twitter(X)</h1>
+            </div>
+          </a>
+        </div>
 
         {/* Discord */}
-        <div className="flex items-center gap-1 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
-          <svg
-            className="w-6 h-6 lg:w-6 lg:h-6"
-            viewBox="0 0 38 28"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M23.91 0.60791C23.4836 1.24743 23.2705 1.88696 23.0573 2.52648C20.286 2.10013 17.5147 2.10013 14.7435 2.52648C14.5303 1.88696 14.1039 1.24743 13.8908 0.60791C11.3327 1.03426 8.77459 1.88696 6.42967 2.95283C1.52665 9.77442 0.247603 16.596 0.887127 23.4176C3.6584 25.5493 6.64284 27.0416 10.0536 27.8943C10.6932 26.8284 11.5459 25.7625 11.9722 24.6966C10.9063 24.2703 9.84046 23.8439 8.98776 23.2044C9.20093 23.2044 9.41411 22.7781 9.84046 22.5649C15.5962 25.3362 22.2046 25.3362 28.1735 22.5649C28.3866 22.5649 28.5998 22.9912 29.0262 23.2044C27.9603 23.8439 27.1076 24.2703 26.0417 24.6966C26.6812 25.7625 27.3208 26.8284 27.9603 27.8943C31.1579 26.8284 34.3555 25.3362 37.1268 23.4176C37.9795 15.7433 35.8478 8.92172 31.7974 2.95283C29.4525 1.88696 26.8944 1.03426 24.3363 0.60791H23.91ZM12.8249 19.1541C11.1195 19.1541 9.62728 17.4487 9.62728 15.5301C9.62728 13.6116 11.1195 11.9062 12.8249 11.9062C14.5303 11.9062 16.0225 13.6116 16.0225 15.5301C16.0225 17.4487 14.5303 19.1541 12.8249 19.1541ZM24.7627 19.1541C23.0573 19.1541 21.5651 17.4487 21.5651 15.5301C21.5651 13.6116 23.0573 11.9062 24.7627 11.9062C26.4681 11.9062 27.9603 13.6116 27.9603 15.5301C27.9603 17.4487 26.4681 19.1541 24.7627 19.1541Z"
-              fill="white"
-            />
-          </svg>
-          <h1 className="hidden desktop-940:block lg:block">Discord</h1>
-        </div>
+        <a
+          href="https://discord.gg/XdX63jJW64"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary  font-medium"
+        >
+          <div className="flex items-center gap-1 sm:gap-[2px] text-sm sm:text-base lg:gap-2 lg:text-lg whitespace-nowrap">
+            <svg
+              className="w-6 h-6 lg:w-6 lg:h-6"
+              viewBox="0 0 38 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M23.91 0.60791C23.4836 1.24743 23.2705 1.88696 23.0573 2.52648C20.286 2.10013 17.5147 2.10013 14.7435 2.52648C14.5303 1.88696 14.1039 1.24743 13.8908 0.60791C11.3327 1.03426 8.77459 1.88696 6.42967 2.95283C1.52665 9.77442 0.247603 16.596 0.887127 23.4176C3.6584 25.5493 6.64284 27.0416 10.0536 27.8943C10.6932 26.8284 11.5459 25.7625 11.9722 24.6966C10.9063 24.2703 9.84046 23.8439 8.98776 23.2044C9.20093 23.2044 9.41411 22.7781 9.84046 22.5649C15.5962 25.3362 22.2046 25.3362 28.1735 22.5649C28.3866 22.5649 28.5998 22.9912 29.0262 23.2044C27.9603 23.8439 27.1076 24.2703 26.0417 24.6966C26.6812 25.7625 27.3208 26.8284 27.9603 27.8943C31.1579 26.8284 34.3555 25.3362 37.1268 23.4176C37.9795 15.7433 35.8478 8.92172 31.7974 2.95283C29.4525 1.88696 26.8944 1.03426 24.3363 0.60791H23.91ZM12.8249 19.1541C11.1195 19.1541 9.62728 17.4487 9.62728 15.5301C9.62728 13.6116 11.1195 11.9062 12.8249 11.9062C14.5303 11.9062 16.0225 13.6116 16.0225 15.5301C16.0225 17.4487 14.5303 19.1541 12.8249 19.1541ZM24.7627 19.1541C23.0573 19.1541 21.5651 17.4487 21.5651 15.5301C21.5651 13.6116 23.0573 11.9062 24.7627 11.9062C26.4681 11.9062 27.9603 13.6116 27.9603 15.5301C27.9603 17.4487 26.4681 19.1541 24.7627 19.1541Z"
+                fill="white"
+              />
+            </svg>
+            <h1 className="hidden desktop-940:block lg:block">Discord</h1>
+          </div>
+        </a>
+
+
       </div>
     </div>
   );
